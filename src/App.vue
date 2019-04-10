84c42bcd753657e78fe14c58ccc9370c5d069778
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-09 21:40:53 -0300

    Working with events with vue, passing data between the components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    Adivinhe um número de 0 a 100:
+    <Entrada v-on:mudou="atualizaNumero"/>
+    <br>
+    <Button texto="Checar número" classe="btn btn-outline-primary" alerta="Este é um alerta" fundo="" v-on:clicou="this.checarNumero"/>
+    <br>
+    <Button texto="Reiniciar" classe="btn btn-outline-danger" fundo="" v-on:clicou="this.reiniciar"/>
+    <div id="txtSaida" style="color: green"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Entrada from './components/Entrada.vue'
+import Button from './components/Button.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Entrada,
+    Button
+  },
+  data(){
+    return { valor: "", numero: Math.floor(Math.random() * 101)}
+  },
+  methods: {
+    //Recebendo o valor de outro componente
+    atualizaNumero(txtNum){
+      this.valor = txtNum;
+    },
+    checarNumero(){
+      if(this.valor == this.numero){
+        alert("Acertou mizeravi!");
+      }else if(this.valor < this.numero){
+        alert("Tente um número maior.");
+      }else if(this.valor > this.numero){
+        alert("Tente um número menor");
+      }else{
+        alert("Entrada inválida");
+      }
+    },
+    reiniciar(){
+      this.numero = Math.floor(Math.random() * 101);
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+      @import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+      #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+      }
 </style>
---
+++ src/components/Entrada.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="col-md-12">
+            <p></p>
+            <input type="text" v-model="txtNum" v-on:change="enter"/>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+       return { txtNum: "" }
+    },methods:{
+        enter(){
+            this.$emit("mudou", this.txtNum);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
+
